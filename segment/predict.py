8b6f37a0bbcc47efd0df484d1e4d2add052b784b
Peggy <peggy1210.huang@gmail.com>
2024-07-31 14:30:36 +0800

    Update predict.py

--- segment/predict.py
+++ segment/predict.py
@@ -99,6 +99,7 @@ def run(
         with dt[1]:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred, proto = model(im, augment=augment, visualize=visualize)[:2]
+            # Issues on https://github.com/WongKinYiu/yolov9/issues/233
             proto = proto[2]
 
         # NMS
